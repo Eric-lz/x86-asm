ea61ee5a6ff2564a6cca34233cbb168f35198195
Eric Peracchi Pisoni <ericperacchi@gmail.com>
2024-02-01 02:12:56 -0300

    Print newline character after loop Better comments

--- loop.asm
+++ loop.asm
@@ -4,21 +4,19 @@ SYS_WRITE   equ 4
 STDIN       equ 0
 STDOUT      equ 1
 
-section .data
-    LF db 0xA           ;newline character
-
 section .bss
-    num resb 1
+    num resb 2      ;one byte for numbers and one for newline character
 
 section .text
     global _start
 
 _start:
-    mov byte [num],'0'  ;first number to print
-    mov ecx,10          ;loop instruction uses ECX as counter
+    mov byte [num+1],0xA    ;set newline character (0xA) in address next to num
+    mov byte [num],'0'      ;initialize first number to print
+    mov ecx,10              ;loop instruction uses ECX as counter
 
-l1:   ;print loop
-    push rcx            ;store ECX (RCX in 64-bit mode)
+l1: ;print loop
+    push rcx    ;store ECX (RCX in 64-bit mode)
     
     ;print num
     mov eax,SYS_WRITE
@@ -32,11 +30,18 @@ l1:   ;print loop
     inc byte [num]
 
     ;get ECX counter back for the loop
-    pop rcx
-    loop l1
+    pop rcx     ;ECX is RCX in elf64 mode
+    loop l1     ;loop 10 times
 
-    ;exit
+    ;print newline
+    mov eax,SYS_WRITE
+    mov ebx,STDOUT
+    mov ecx,num+1       ;where the newline 0xA character is stored
+    mov edx,1           ;1 character = 1 byte
+    int 0x80
+
+    ;exit signal
     mov eax,SYS_EXIT
-    xor ebx,ebx
+    xor ebx,ebx         ;exit code 0
     int 0x80
     
